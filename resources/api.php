6dd7ccf5e16b64b3c1267b6c4962c00770de45b8
Jordy van Kuijk <jvankuijk@me.com>
2014-02-07 12:55:47 +0100

    Update api.php

--- resources/api.php
+++ resources/api.php
@@ -4,6 +4,7 @@ require_once 'config.php';
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
+header('Access-Control-Allow-Origin: *');  
 /*
  * Sqwiggle API wrapper
 */
