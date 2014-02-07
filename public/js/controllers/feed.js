bfa0427509af6495680167795a64885becb1e365
DyelKassaKoumba <dyel.kassakoumba@gmail.com>
2014-02-07 13:12:26 +0100

    Feed Update

--- public/js/controllers/feed.js
+++ public/js/controllers/feed.js
@@ -2,18 +2,29 @@
 
 angular.module('sqwiggle-feed.system').controller('FeedController', ['$scope', '$http', function ($scope, $http) {
 	$scope.messages = [];
+	$scope.users = [];
 	$scope.menuactive = false;
 	$scope.togglemenu = function(){
 		$scope.menuactive = !$scope.menuactive;
 	}
 	$scope.getMessages = function() {
-		$http.get('/resources/api.php', {
+		$http.get('http://www.everywhere.is/making/sqwiggle/resources/api.php', {
     		params: {
     			endpoint: 'messages'
     		}
     	}).success(function(e) {
     		console.log(e);
     		$scope.messages = e;
+    	});
+	}
+	$scope.getUsers = function() {
+		$http.get('http://www.everywhere.is/making/sqwiggle/resources/api.php', {
+    		params: {
+    			endpoint: 'users'
+    		}
+    	}).success(function(e) {
+    		console.log(e);
+    		$scope.users = e;
     	});
 	}
 }]); 
