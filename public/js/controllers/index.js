acccc2cf9328efcf070a6db75940d809523eb5cc
Jordy van Kuijk <jvankuijk@me.com>
2014-02-07 02:54:34 +0100

    worked on php wrapper

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('sqwiggle-feed.system').controller('IndexController', ['$scope', '$interval', function ($scope, $interval) {
+angular.module('sqwiggle-feed.system').controller('IndexController', ['$scope', '$interval', '$http', function ($scope, $interval, $http) {
     $scope.dots = '...';
     $scope.animateDots = function() {
     	var dots = $scope.dots;
