5e4c027dc2a2a3f28ba702bacb8f35312911539b
Jordy van Kuijk <mail.npsoftware.nl>
2014-02-19 22:10:23 +0100

    cleaned pages branch

--- public/js/filters.js
+++
@@ -1,8 +0,0 @@
-'user strict'
-
-// filter for use in TRUSTED html sources only
-angular.module('sqwiggle-feed.filters').filter('unsafe', function($sce) {
-	return function(val) {
-		return $sce.trustAsHtml(val);
-	}
-});
